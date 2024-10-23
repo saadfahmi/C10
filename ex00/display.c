ae6b37dc2f9dd47f8b71c5c0ff79bc2a38e6e4ec
saadfahmitest <s.fahmi2618@uca.ac.ma>
2024-10-23 17:06:08 +0100

    100%

---
+++ ex00/display.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <fcntl.h>
+int ft_display(char *file){
+
+
+    int r;
+    int f;
+    char a;
+    f = open(file, 0);
+    if(f == -1)
+    return 0;
+    while((r = read (f, &a, 1))){
+
+        if(r == -1)
+        return(0);
+        write(1, &a, 1);
+    
+
+
+    }
+    close(f);
+    return(1);
+    
+}
+int main(int argc, char **argv)
+{
+    if(argc == 1)
+    write(2, ".file name missing\n", 19);
+    else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else
+    {
+        if(!ft_display(argv[1])){
+
+
+            write(2, "Cannot read the file.\n", 23);
+
+
+
+
+    }
+
+    return(0);
+
+
+}
+}
+
